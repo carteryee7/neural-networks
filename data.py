bec4ef10c83c579b4ba316b443dc90e3bf8ed315
carteryee7 <carter.yee07@gmail.com>
2026-05-29 19:20:52 -0400

    iris prediction model

---
+++ data.py
@@ -0,0 +1,84 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import ssl
+import urllib.request
+from sklearn.model_selection import train_test_split
+import torch
+import torch.nn as nn
+from model import model
+
+url = 'https://gist.githubusercontent.com/netj/8836201/raw/6f9306ad21398ea43cba4f7d537619d0e07d5ae3/iris.csv'
+context = ssl._create_unverified_context()
+with urllib.request.urlopen(url, context=context) as response:
+    my_df = pd.read_csv(response)
+
+my_df['variety'] = my_df['variety'].map({
+    'Setosa': 0,
+    'Versicolor': 1,
+    'Virginica': 2,
+}).astype('int64')
+
+
+# Train Test Split!  Set X, y
+X = my_df.drop('variety', axis=1)
+y = my_df['variety']
+
+# Convert these to numpy arrays
+X = X.values
+y = y.values
+
+# Train Test Split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=41)
+
+# Convert X features to float tensors
+X_train = torch.FloatTensor(X_train)
+X_test = torch.FloatTensor(X_test)
+
+# Convert y labels to tensors long
+y_train = torch.LongTensor(y_train)
+y_test = torch.LongTensor(y_test)
+
+
+# Set the criterion of model to measure the error, how far off the predictions are from the data
+criterion = nn.CrossEntropyLoss()
+# Choose Adam Optimizer, lr = learning rate (if error doesn't go down after a bunch of iterations (epochs), lower our learning rate)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+
+
+# Train our model!
+# Epochs? (one run thru all the training data in our network)
+epochs = 100
+losses = []
+for i in range(epochs):
+    # Go forward and get a prediction
+    y_pred = model.forward(X_train) # Get predicted results
+
+    # Measure the loss/error, gonna be high at first
+    loss = criterion(y_pred, y_train) # predicted values vs the y_train
+
+    # Keep Track of our losses
+    losses.append(loss.detach().numpy())
+
+    # print every 10 epoch
+    if i % 10 == 0:
+        print(f'Epoch: {i} and loss: {loss}')
+
+    # Do some back propagation: take the error rate of forward propagation and feed it back
+    # thru the network to fine tune the weights
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+plt.plot(range(epochs), losses)
+plt.ylabel("loss/error")
+plt.xlabel('Epoch')
+plt.show()
+
+
+
+# Evaluate Model on Test Data Set (validate model on test set)
+with torch.no_grad():  # Basically turn off back propogation
+    y_eval = model.forward(X_test) # X_test are features from our test set, y_eval will be predictions
+    loss = criterion(y_eval, y_test) # Find the loss or error
+
+print(loss)
