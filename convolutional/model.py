4a243851a25259556c1c52b8d74be309531ec712
carteryee7 <carter.yee07@gmail.com>
2026-06-01 16:57:59 -0400

    multi-layered cnn

--- convolutional/model.py
+++ convolutional/model.py
@@ -15,9 +15,12 @@ class CNNModel(nn.Module):
         return self.fc(x)
 
 torch.manual_seed(67)
+
+# v1.0
 #model = nn.Sequential(nn.Conv2d(1, 16, 3), nn.ReLU(), nn.MaxPool2d(2, 2), nn.Flatten(), nn.Linear(16 * 13 * 13, 10, bias=True))
 
-
+"""
+# v2.0
 model = nn.Sequential(
     nn.Conv2d(1, 32, 3),
     nn.ReLU(),
@@ -27,4 +30,21 @@ model = nn.Sequential(
     nn.ReLU(),
     nn.Dropout(0.25),       # Regularize the fully-connected layer
     nn.Linear(128, 10)
+)
+"""
+
+# v3.0 (full CNN)
+
+model = nn.Sequential(
+    nn.Conv2d(1, 32, 3, padding=1),   # 28x28
+    nn.ReLU(),
+    nn.MaxPool2d(2, 2),               # 14x14
+    nn.Conv2d(32, 64, 3, padding=1),  # 14x14
+    nn.ReLU(),
+    nn.MaxPool2d(2, 2),               # 7x7
+    nn.Flatten(),                     # 64*7*7 = 3136
+    nn.Linear(64 * 7 * 7, 128),
+    nn.ReLU(),
+    nn.Dropout(0.25),
+    nn.Linear(128, 10),
 )
