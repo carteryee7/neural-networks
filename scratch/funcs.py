460fc199e3baf461dacde4d0b812e9b164be652c
carteryee7 <carter.yee07@gmail.com>
2026-06-12 17:58:13 -0400

    back prop

--- scratch/funcs.py
+++ scratch/funcs.py
@@ -2,24 +2,31 @@ import numpy as np
 import random
 
 class Linear():
-    def __init__(self, in_features: int, out_features: int, bias=True):
+    def __init__(self, in_features: int, out_features: int, activation, bias=True):
         self.in_features = in_features
         self.out_features = out_features
+        self.activation = activation
         self.bias = bias
         self.reset_params()
+        
     
     def __call__(self, x):
-        h, w = x.shape
-
+        _, w = x.shape
+        
         if w == self.in_features:
             z = x @ self.A.transpose() + self.b
-            return z
+            self.x = x
+            self.z = z
+            self.a = self.activation(z)
+            return self.a
         else:
             print(f"Error: input cols ({w}) must be equal to in_features ({self.in_features})")
     
     def reset_params(self):
         self.A = np.array([[random.uniform(-0.5, 0.5) for _ in range(self.in_features)] for _ in range(self.out_features)])
         self.b = np.array([0.0 for _ in range(self.out_features)])
+        self.gradA = np.array([[0.0 for _ in range(self.in_features)] for _ in range(self.out_features)])
+        self.gradb = np.array([0.0 for _ in range(self.out_features)])
 
 
 def relu(x):
@@ -46,3 +53,15 @@ def softmax(x):
     
     return out
 
+def deriv_relu(x):
+    return x > 0
+
+def one_hot_Y(y):
+    one_hot = np.zeros(y.size, y.max() + 1)
+    one_hot[np.arange(y.size), y] = 1
+    return one_hot
+
+def mseLoss(a, y):
+    #n = a.size # same as y
+    loss = (a - y) ** 2
+    return sum(loss)
--- scratch/model.py
+++ scratch/model.py
@@ -1,20 +1,33 @@
-from funcs import Linear, relu, softmax
+from funcs import Linear, relu, softmax, deriv_relu, one_hot_Y
 
 class NN:
     def __init__(self, in_features=11, out_features=4, h1=64, h2=32):
-        self.fc1 = Linear(in_features, h1)
-        self.fc2 = Linear(h1, h2)
-        self.out = Linear(h2, out_features)
+        self.fc1 = Linear(in_features, h1, relu)
+        self.fc2 = Linear(h1, h2, relu)
+        self.out = Linear(h2, out_features, softmax)
     
     def __call__(self, x):  # forward prop
         return self.forward(x)
     
     def forward(self, x):
-        x = relu(self.fc1(x))
-        x = relu(self.fc2(x))
-        output = softmax(self.out(x))
+        x = self.fc1(x)
+        x = self.fc2(x)
+        output = self.out(x)
 
         return output
     
-    def back_prop(self):
+    def back_prop(self, y, alpha):
+        y = one_hot_Y(y)
+        m = y.shape[0]
+
+        dz = self.out.a - y
+
+        layers = [(self.out, self.fc2.z), (self.fc2, self.fc1.z), (self.fc1, self.fc1.x)]
         
+        for layer, prev_z in layers:
+            gradA = dz.transpose() @ layer.x / m
+            gradb = dz.mean(axis=0)
+            da = dz @ layer.A
+            dz = da * deriv_relu(prev_z)
+            layer.A = layer.A - (alpha * gradA)
+            layer.b = layer.b - (alpha * gradb)
